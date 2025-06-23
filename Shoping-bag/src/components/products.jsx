fc7eb04afbe2c74126a46f99e8b912be83a78e4f
yahya-saeed <yahyasaeed171@gmail.com>
2025-06-23 19:45:40 +0500

    card backgroud css changed hight value to auto

--- Shoping-bag/src/components/products.jsx
+++ Shoping-bag/src/components/products.jsx
@@ -16,7 +16,7 @@ export default function Products() {
   };
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
+    fetch("https://fakestoreapi.com/products  ")
       .then((res) => res.json())
       .then((data) => setProducts(data))
       .catch((err) => {
